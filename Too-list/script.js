2aa07c25e80d46a7de9a74574e8347e5df4ec857
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-06-13 23:53:37 +0530

    update code

--- Too-list/script.js
+++ Too-list/script.js
@@ -1,19 +1,37 @@
-const inp = document.querySelector("input")
-const btn = document.querySelector("#add")
-const todoBox = document.querySelector('.too-doo')
+const inp = document.querySelector("input");
+const btn = document.querySelector("#add");
+const todoBox = document.querySelector(".too-doo");
 
+btn.addEventListener("click", () => {
+  const value = inp.value;
 
-btn.addEventListener('click',()=>{
-    const value = inp.value;
-
-    if(value.trim() === "") return;
-    todoBox.innerHTML += `
+  if (value.trim() === "") return;
+  todoBox.innerHTML += `
         <div class="li">
-          <h1>${value}</h1>
-          <div class="btn">
-            <button>Edit</button>
-            <button>Delete</button>
-          </div>
-        </div>`
-    inp.value = ""
+  <h1>${value}</h1>
+  <div class="btn">
+    <button class="edit">Edit</button>
+    <button class="delete">Delete</button>
+  </div>
+</div>`;
+  inp.value = "";
+});
+
+
+todoBox.addEventListener('click',(e)=>{
+    if(e.target.classList.contains("delete")){
+        e.target.closest(".li").remove();
+    }
 })
+todoBox.addEventListener("click", (e) => {
+    if (e.target.classList.contains("edit")) {
+
+        const task = e.target.closest(".li").querySelector("h1");
+
+        const updatedTask = prompt("Edit Task", task.textContent);
+
+        if (updatedTask !== null && updatedTask.trim() !== "") {
+            task.textContent = updatedTask;
+        }
+    }
+});
