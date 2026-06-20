ea778cc805e498b5b95557a3c3c83ac3baf5f1f1
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-06-20 20:57:58 +0530

    update code

--- Random _box/random.js
+++ Random _box/random.js
@@ -1,13 +1,24 @@
 const btn = document.querySelector("button");
 const main = document.querySelector("main");
 const box = document.querySelector(".box");
+const timer = document.querySelector(".timer");
 
-btn.addEventListener('click', ()=>{
+let time = 0;
+let interval;
 
-    const eY =  Math.random() * 100;
-    const eX =  Math.random() * 100;
+btn.addEventListener("click", () => {
+  let interval = setInterval(() => {
+    time += 1;
+    timer.innerHTML = time;
 
+    const eY = Math.random() * 100;
+    const eX = Math.random() * 100;
 
-    box.style.top = `${eY}%`
-    box.style.left = `${eX}%`
-}) 
+    box.style.top = `${eY}%`;
+    box.style.left = `${eX}%`;
+  }, 1000);
+
+  setTimeout(() => {
+    clearInterval(interval);
+  }, 10000);
+});
