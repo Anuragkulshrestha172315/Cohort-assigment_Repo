9a60000e939ed1265c9bc4bb790d244c9c7867c0
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-06-20 23:53:23 +0530

    update code

--- Random _box/random.js
+++ Random _box/random.js
@@ -1,30 +1,49 @@
 const btn = document.querySelector("button");
 const main = document.querySelector("main");
-const box = document.querySelector(".box");
+const overlay = document.querySelector(".overlay")
+const box = document.createElement("div");
+box.classList.add("box");
 const timer = document.querySelector(".timer");
 
 let time = 0;
 let interval;
-const randomBox =()=>{
-    time += 1;
-    timer.innerHTML = time;
 
-    const eY = Math.random() * 100;
-    const eX = Math.random() * 100;
+const randomColor = () => {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
 
-    box.style.top = `${eY}%`;
-    box.style.left = `${eX}%`;
-}
+  return `rgb(${r}, ${g}, ${b}) `;
+};
+
+const randomBox = () => {
+  box.style.backgroundColor = randomColor();
+
+  main.append(box);
+
+  time += 1;
+  timer.innerHTML = time;
+
+  const mainH = main.clientHeight - box.offsetHeight;
+  const mainW = main.clientWidth- box.offsetWidth;
+
+  const eY = Math.random() * mainH;
+  const eX = Math.random() * mainW;
+
+  box.style.top = `${eY}px`;
+  box.style.left = `${eX}px`;
+};
 
 btn.addEventListener("click", () => {
-    clearInterval(interval);
-    
-   interval = setInterval(() => {
-    randomBox()
-    
+  clearInterval(interval);
+  randomBox();
+
+  interval = setInterval(() => {
+    randomBox();
   }, 1000);
 
   setTimeout(() => {
-    clearInterval(interval)
+    clearInterval(interval);
+    overlay.style.display = "flex"
   }, 10000);
 });
