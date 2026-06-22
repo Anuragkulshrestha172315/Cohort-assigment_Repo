a386f3dd3da1cfef17e5fb96f517cb534c470e71
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-06-22 19:34:34 +0530

    update code

--- Task_Manager/task.js
+++ Task_Manager/task.js
@@ -6,8 +6,30 @@ const right = document.querySelector(".right");
 const form = document.querySelector("form")
 const cross = document.querySelector(".crosses")
 const add = document.querySelector(".add")
+const taskDiv = document.querySelector(".allTask")
 
 let taskArr = [];
+let updateindex = null
+let ui=()=>{
+    taskDiv.innerHTML ="";
+    taskArr.forEach((elem,index)=>{
+        taskDiv.innerHTML += `<div class="task">
+                        <input id="checkBox" type="checkbox">
+                        <div class="category"><p>${elem.category}</p></div>
+                        <div class="title">
+                            <h1>${elem.taskTitle}</h1>
+                            <p>${elem.aboutTask}</p>
+                            <p> <i class="ri-calendar-line"></i>${elem.date}</p>
+                        </div>
+                        <div class="btn">
+                            <button id="view">View Task <i class="ri-arrow-down-s-line"></i></button>
+                            <button onclick = "updateTask('${elem.taskTitle}')" id="edit"><i class="ri-pencil-ai-2-line"></i></button>
+                            <button onclick = "deleteTask('${index}')"  id="delete"><i class="ri-delete-bin-5-line"></i></button>
+                        </div>
+                    </div>`
+    })
+    
+}
 
 add.addEventListener('click',()=>{
     form.style.display = "flex"
@@ -16,7 +38,6 @@ add.addEventListener('click',()=>{
 cross.addEventListener('click',()=>{
     form.style.display = "none"
 })
-
 
 dark.addEventListener('click', ()=>{
     main.classList.toggle('bright')
@@ -33,10 +54,11 @@ form.addEventListener('submit',(event)=>{
     let category = event.target[0].value;
     let taskTitle = event.target[1].value;
     let aboutTask = event.target[2].value;
-    let date = event.target[4].value;
+    let date = event.target[3].value;
 
     if(category.trim() ==="" || taskTitle.trim() === "" || aboutTask.trim() === "" || date.trim() === ""){
         alert("Please Enter the all feild!")
+        return;
     }
     
     let obj = {
@@ -45,7 +67,31 @@ form.addEventListener('submit',(event)=>{
         aboutTask,
         date
     }
-    console.log(obj);
-    
+    if(updateindex != null){
+        taskArr[updateindex] = obj;
+        updateindex = null
+    }else{
+        taskArr.push(obj)
+    }
+    ui();
+    console.log(taskArr);
+    form.reset();
+    form.style.display = "none"
 
 })
+
+const updateTask = (name) => {
+    form.style.display = "flex"
+    let task = taskArr.find((elem)=> elem.taskTitle === name)
+    updateindex = taskArr.findIndex((elem)=> elem.taskTitle === name)
+    form[0].value = task.category
+    form[1].value = task.taskTitle
+    form[2].value = task.aboutTask
+    form[3].value = task.date
+
+}
+
+const deleteTask = (index)=>{
+    taskArr.splice(index,1)
+    ui()
+}
