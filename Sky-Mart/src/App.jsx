9e69374829f93355ba9250d902e24c5c919e2d2c
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-07-18 16:05:08 +0530

    update code

--- Sky-Mart/src/App.jsx
+++ Sky-Mart/src/App.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Sky_Mart</h1>
+      <div>
+        <nav>
+          <div>home</div>
+        </nav>
+      </div>
     </div>
   )
 }
