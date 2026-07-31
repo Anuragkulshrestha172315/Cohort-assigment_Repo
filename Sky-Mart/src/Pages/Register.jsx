a6adc408891d2f1cf9622f9ea07d00aa4a315d78
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-07-31 19:35:58 +0530

    update

--- Sky-Mart/src/Pages/Register.jsx
+++ Sky-Mart/src/Pages/Register.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import { User, Mail, Lock, Eye, EyeOff, Zap, ArrowRight } from "lucide-react";
 import { useNavigate } from "react-router";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
- 
-  let navigate = useNavigate()
+  let {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  let formSubmit = (data) => {};
+
+  let navigate = useNavigate();
 
   return (
     <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center px-4 py-10 sm:py-16">
@@ -25,54 +34,77 @@ const Register = () => {
           Join SkyMart and start shopping
         </p>
 
-        <form  className="space-y-4">
+        <form onSubmit={handleSubmit(formSubmit)} className="space-y-4">
           <div className="relative">
             <User className="absolute left-4 top-1/2 -translate-y-1/2 w-[18px] h-[18px] text-neutral-500" />
             <input
+              {...register("name", {
+                required: "Name is required",
+              })}
               type="text"
               placeholder="Full name"
               className="w-full bg-neutral-900 border border-neutral-800 rounded-xl py-4 pl-11 pr-4 text-sm text-white placeholder-neutral-500 outline-none focus:border-lime-400 transition-colors"
             />
+            {errors.name && <p>{errors.name.message}</p>}
           </div>
+
           <div className="relative">
             <Mail className="absolute left-4 top-1/2 -translate-y-1/2 w-[18px] h-[18px] text-neutral-500" />
             <input
+              {...register("email", {
+                required: "Email is required",
+              })}
               type="email"
               placeholder="Email address"
               className="w-full bg-neutral-900 border border-neutral-800 rounded-xl py-4 pl-11 pr-4 text-sm text-white placeholder-neutral-500 outline-none focus:border-lime-400 transition-colors"
             />
+            {errors.email && <p>{errors.email.message}</p>}
           </div>
 
           <div className="relative">
             <Lock className="absolute left-4 top-1/2 -translate-y-1/2 w-[18px] h-[18px] text-neutral-500" />
             <input
-              type= "password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "At least 6 characters required",
+                },
+              })}
+              type="password"
               placeholder="Password (min 6 chars)"
               className="w-full bg-neutral-900 border border-neutral-800 rounded-xl py-4 pl-11 pr-11 text-sm text-white placeholder-neutral-500 outline-none focus:border-lime-400 transition-colors"
             />
+            {errors.password && <p>{errors.password.message}</p>}
             <button
               type="button"
               className="absolute right-4 top-1/2 -translate-y-1/2 text-neutral-500 hover:text-neutral-300 transition-colors"
               aria-label="Toggle password visibility"
-            >
-            
-            </button>
+            ></button>
           </div>
 
           <div className="relative">
             <Lock className="absolute left-4 top-1/2 -translate-y-1/2 w-[18px] h-[18px] text-neutral-500" />
             <input
+              {...register("confirm_password", {
+                required: "Confirm Password is required",
+                minLength: {
+                  value: 6,
+                  message: "At least 6 characters required",
+                },
+              })}
               type="password"
               placeholder="Confirm password"
               className="w-full bg-neutral-900 border border-neutral-800 rounded-xl py-4 pl-11 pr-11 text-sm text-white placeholder-neutral-500 outline-none focus:border-lime-400 transition-colors"
             />
+            {errors.confirm_password && (
+              <p>{errors.confirm_password.message}</p>
+            )}
             <button
               type="button"
               className="absolute right-4 top-1/2 -translate-y-1/2 text-neutral-500 hover:text-neutral-300 transition-colors"
               aria-label="Toggle confirm password visibility"
-            >
-            
-            </button>
+            ></button>
           </div>
 
           <button
@@ -86,12 +118,16 @@ const Register = () => {
 
         <p className="text-center text-neutral-500 text-sm mt-6">
           Already have an account?{" "}
-          <button onClick={()=>navigate('/login')} href="#" className="text-lime-400 font-semibold hover:underline">
+          <button
+            onClick={() => navigate("/login")}
+            href="#"
+            className="text-lime-400 font-semibold hover:underline"
+          >
             Sign in
           </button>
         </p>
       </div>
     </div>
   );
-}
-export default Register
+};
+export default Register;
