6a004fe0c80bfa1040726ebbf234b5ef2352e173
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-08-05 22:51:17 +0530

    update

--- Sky-Mart/src/Pages/Login.jsx
+++ Sky-Mart/src/Pages/Login.jsx
@@ -110,7 +110,7 @@ const Login = ()=> {
                 required: "Password is required",
                 minLength: {
                   value: 6,
-                  message: "At Least 6 characters Reuired",
+                  message: "At Least 6 characters Ruired",
                 },
               })}
                 type="password"
