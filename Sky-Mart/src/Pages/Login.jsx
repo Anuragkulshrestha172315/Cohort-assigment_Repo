b542a9d73e3dfc597e9cafe8f342fbb284d1df1d
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-08-14 23:16:33 +0530

    update

--- Sky-Mart/src/Pages/Login.jsx
+++ Sky-Mart/src/Pages/Login.jsx
@@ -54,7 +54,7 @@ const Login = ()=> {
               <span className="block text-lime-400">Today.</span>
             </h1>
             <p className="mt-5 sm:mt-6 text-neutral-400 text-sm sm:text-base leading-relaxed max-w-md">
-              Thousands of products, lightningfast delivery, and prices that
+              Thousands of products, lightning Fast delivery, and prices that
               make your wallet happy.
             </p>
 
