20b609afd62728797d3f970035316a34a28c4820
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-09-01 23:25:37 +0530

    update

--- Sky-Mart/src/Pages/Register.jsx
+++ Sky-Mart/src/Pages/Register.jsx
@@ -38,7 +38,7 @@ const Register = () => {
           Create account
         </h2>
         <p className="text-neutral-500 text-sm mb-8">
-          Join SkyMart and start Soplingddddkk
+          Join SkyMart and start Soplingddddkkkk
         </p>
 
         <form onSubmit={handleSubmit(formSubmit)} className="space-y-4">
